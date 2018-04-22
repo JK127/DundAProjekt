a1d2adff5262b6c585af69ecdbfef5d211da44ac
jonasklein <jonasklein@192.168.0.221>
2018-04-22 19:07:57 +0200

    Zugriff auf db Testweise eingebaut

--- DAProjekt/src/main/java/de/jonasklein/HomePage.java
+++ DAProjekt/src/main/java/de/jonasklein/HomePage.java
@@ -2,17 +2,39 @@ package de.jonasklein;
 
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.apache.wicket.markup.html.WebPage;
 
 public class HomePage extends WebPage {
+	private String USR = "root";
+	private String PWD = "Pa$$w0rd";
+	private String CONSTRG = "jdbc:mysql://localhost:3306/DAProjekt";
+	private String A = "SELECT Spalte FROM TestTabelle WHERE idTestTabelle = 1";
+	private ResultSet rs;
+	private String WRT;
+	
+
 	private static final long serialVersionUID = 1L;
 
-	public HomePage(final PageParameters parameters) {
+	public HomePage(final PageParameters parameters) throws SQLException {
 		super(parameters);
 
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+		try(Connection c = DriverManager.getConnection(CONSTRG, USR, PWD); PreparedStatement ps = c.prepareStatement(A)){
+			rs = ps.executeQuery();
+			rs.next();
+			WRT = rs.getString(1);
+		}
+		
+		add(new Label("version", WRT));
 
 		// TODO Add your page's components here
 
+		
     }
 }
