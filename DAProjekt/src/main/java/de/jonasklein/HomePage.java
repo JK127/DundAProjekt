e3d7ea97d738ca7ebdd42970d860f9cb4232b82c
jonasklein <jonasklein@192.168.0.221>
2018-04-22 21:31:41 +0200

    Zwischenstand

--- DAProjekt/src/main/java/de/jonasklein/HomePage.java
+++ DAProjekt/src/main/java/de/jonasklein/HomePage.java
@@ -2,6 +2,9 @@ package de.jonasklein;
 
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.PropertyModel;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -17,7 +20,8 @@ public class HomePage extends WebPage {
 	private String CONSTRG = "jdbc:mysql://localhost:3306/DAProjekt";
 	private String A = "SELECT Spalte FROM TestTabelle WHERE idTestTabelle = 1";
 	private ResultSet rs;
-	private String WRT;
+	private String WRT = "TEST";
+	private int message = 123;
 	
 
 	private static final long serialVersionUID = 1L;
@@ -25,16 +29,38 @@ public class HomePage extends WebPage {
 	public HomePage(final PageParameters parameters) throws SQLException {
 		super(parameters);
 
-		try(Connection c = DriverManager.getConnection(CONSTRG, USR, PWD); PreparedStatement ps = c.prepareStatement(A)){
-			rs = ps.executeQuery();
-			rs.next();
-			WRT = rs.getString(1);
-		}
+//		try(Connection c = DriverManager.getConnection(CONSTRG, USR, PWD); PreparedStatement ps = c.prepareStatement(A)){
+//			rs = ps.executeQuery();
+//			rs.next();
+//			WRT = rs.getString(1);
+//		}
 		
 		add(new Label("version", WRT));
+		
+		
+		PropertyModel<String> messageModel = new PropertyModel<>(this, "message");
+		PropertyModel<String> sqrtMessageModel = new PropertyModel<>(this, "quad");
+		
+		 Form<?> form = new Form("form");
+	        form.add(new TextField<>("msgInput", messageModel));
+	        add(form);
 
+	        add(new Label("msg", sqrtMessageModel));
 		// TODO Add your page's components here
 
 		
     }
+
+	public int quad() {
+		return message * message;
+	}
+	public int getMessage() {
+		return message;
+	}
+
+	public void setMessage(int message) {
+		this.message = message;
+	}
+
+
 }
